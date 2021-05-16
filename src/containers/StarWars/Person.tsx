8bf7bfe760a18abb95e4493b5d6c356374dee24f
bert@catsburg.com <bert@catsburg.com>
2021-05-16 17:58:08 +0200

    Next up: Movies

--- src/containers/StarWars/Person.tsx
+++ src/containers/StarWars/Person.tsx
@@ -2,19 +2,39 @@ import React from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { PersonNumberState } from 'store/starwars/personNumberState';
 import { getStarWarsPerson } from 'store/starwars/getPerson';
+import { useForm } from 'react-hook-form';
+import { Button, Typography } from '@material-ui/core';
 
 const SWPerson: React.FC = () => {
     const [personNumber, setPersonNumber] = useRecoilState(PersonNumberState);
     const person = useRecoilValue(getStarWarsPerson);
 
-    const handleChange = (event: any) => {
-        setPersonNumber(event.target.value);
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data: any) => {
+        setPersonNumber(data.personNumber);
     };
+    const onFocusInput = (event: any) => event.target.select();
 
     return (
         <React.Fragment>
-            <input type={'text'} value={personNumber} onChange={handleChange} />
-            <p>Person = {person.name}</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    type={'text'}
+                    defaultValue={personNumber}
+                    onFocus={onFocusInput}
+                    {...register('personNumber')}
+                />
+                <br />
+                <Button type={'submit'} variant={'contained'}>
+                    Get Data
+                </Button>
+            </form>
+
+            <Typography variant="h5" component="h2" color={'primary'}>
+                Person = {person.name}
+            </Typography>
+
             <ul>
                 {person.films.map((p: any) => {
                     return <li key={p}>{p}</li>;
